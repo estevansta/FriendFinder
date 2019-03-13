f061d1b50d0d89c4ee478419d052dd8db7b7f79b
estevansta <cruz@therrb.co>
2019-03-12 22:58:38 -0500

    Basic Layout for express;  + html and routing

---
+++ server.js
@@ -0,0 +1,30 @@
+// require npm express and basic properties for the express server
+var express = require("express");
+
+// our express server in a variable
+var app = express();
+
+// Initial port 
+var PORT = process.env.PORT || 8080;
+
+// using urlendcoded and json to help in parsing the data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
